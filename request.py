16f3328c93bd16cbcb1f67d3c71178ef522b3f5e
Eric O'Brien <cireneirbo@gmail.com>
2025-09-21 10:45:37 -0400

    Create request.py

---
+++ request.py
@@ -0,0 +1,34 @@
+import requests
+
+def get_requests():
+  #the api endpoint
+  url = 'https://jsonplaceholder.typicode.com/posts'
+
+  try:
+    #attempt the api request
+    response = requests.get(url)
+
+    #check if a positive return with status code being 200
+    if response.status_code == 200:
+      posts = response.json()
+      return posts
+    else:
+      print('Error code:', response.status_code)
+      return None
+  except requests.exceptions.RequestException as e:
+    print('Error:', e)
+    return None
+
+def main():
+  #call the api
+  posts = get_requests()
+  #if it responds with data
+  if posts:
+    print('First Post Title:', posts[0]['title'])
+    print('First Post Body', posts[0]['body'])
+  #if no data returns
+  else:
+    print('Failed to return the API data...')
+
+if __name__ == '__main__': #this prevents calling any function other than 'main'. prevents bugs based on how python compiles imports.
+  main()
